8cced06bbf6ba2844694b52fbb087ada6314753d
iOleg303 <olegkatrechko46@gmail.com>
2021-11-13 12:05:51 +0200

    classWork-2(fixed)

--- independentWork-2/classWork-2.js
+++ independentWork-2/classWork-2.js
@@ -1,4 +1,42 @@
-/TODO: Вывести  пользователей, у которых в адресе есть число, к примеру
+// const workerExample = {
+//   name: "Igor",
+//   age: 34,
+//   address: {
+//     street: "Lenin",
+//     house: 23,
+//     apartments: 16
+//   },
+//   userBonuses: {
+//     2019: 700,
+//     2020: 750,
+//     2021: 100
+//   },
+//   wage: 500,
+//   hobby: ["chess", "basketball"]
+// };
+
+// TODO: Отсортировать пользователей по уровню зп
+// Вывод: отсортированные зп через запятую
+export const getSortWage = (data) => {};
+
+// TODO: Вывести зп всех пользователей
+// Число (сумма всех зп)
+export const getWageSum = (data) => {};
+
+// TODO: Вывести пользователя с женским именем, знаем, что имя Ekaterina
+export const getWomenName = (data) => {};
+
+// TODO: Вывести самого молодого и самого старшего пользователя
+// Вывод: "Самого младшего пользователя зовут {...} и ему ${age}.
+// Самого старшего пользователя зовут {...} и ему ${age}."
+export const getMinMaxUserAge = (data) => {};
+
+// TODO: Вывести кол-во пользователей, играющих в баскетбол
+// Элемент выглядит как "basketball"
+// Вывод: "В баскетбол играют {число} пользователей"
+export const getBasketPlayers = (data) => {};
+
+//TODO: Вывести  пользователей, у которых в адресе есть число, к примеру
 // {street: "8 March"}
 // Вывод: "По адресу {...} живут пользователи {...}"
 export const getUsersAddress = (data) => {
@@ -27,4 +65,4 @@ export const getDoubleUserBonuses = (data) => {
     names = names.slice(0,-2) + ' ';
     bonuses = bonuses.slice(0,-2);
     return `Пользователи ${names} получат соответственно ${bonuses} бонусов.`;
-};
+}
